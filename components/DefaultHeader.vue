1ee50b20962ad35651296af390336623040337c2
Samuel Zwinglio <samuelzwinglio@gmail.com>
2022-07-15 18:30:46 -0300

    remove pnpm

---
+++ components/DefaultHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <section id="header">
+    <div class="container mt-4">
+      <div class="row justify-content-center">
+        <div class="col-lg-6 d-flex justify-content-between">
+          <h1>Logo</h1>
+          <div>
+            <NuxtLink to="/dashboard" class="btn btn-primary">
+              Painel
+            </NuxtLink>
+            <a href="#" class="btn btn-primary disabled" disabled>Perfil</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- pages/weeks/index.vue
+++ pages/weeks/index.vue
@@ -1,20 +1,6 @@
 <template>
   <div>
-    <section id="header">
-      <div class="container mt-4">
-        <div class="row justify-content-center">
-          <div class="col-lg-6 d-flex justify-content-between">
-            <h1>Logo</h1>
-            <div>
-              <NuxtLink to="/dashboard" class="btn btn-primary">
-                Painel
-              </NuxtLink>
-              <a href="#" class="btn btn-primary disabled" disabled>Perfil</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+    <DefaultHeader />
     <section id="weeks">
       <div class="container mt-4">
         <div class="row justify-content-center">
