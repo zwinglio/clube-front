35156bcf5953f25e8ff858524d521b2ec27ae56b
Samuel Zwinglio <samuelzwinglio@gmail.com>
2022-08-20 15:06:06 -0300

    protect dashboard

--- components/DefaultHeader.vue
+++ components/DefaultHeader.vue
@@ -5,7 +5,11 @@
         <div class="col-lg-6 d-flex justify-content-between">
           <nuxt-img src="/img/logo.png" alt="" width="80" format="webp" />
           <div>
-            <NuxtLink to="/dashboard" class="btn btn-primary">
+            <NuxtLink
+              to="/dashboard"
+              class="btn btn-primary"
+              v-if="userRole == 'admin'"
+            >
               Painel
             </NuxtLink>
             <a href="#" class="btn btn-primary disabled" disabled>Perfil</a>
@@ -22,6 +26,15 @@ export default {
   methods: {
     logout() {
       this.$auth.logout();
+    },
+  },
+  computed: {
+    user() {
+      return this.$auth.user;
+    },
+    userRole() {
+      const role = this.$auth.user.roles;
+      return role.map((role) => role.name);
     },
   },
 };
