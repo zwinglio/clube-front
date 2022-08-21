cf845e39d94452a31e72b7e3e1b636d16bb84e86
Samuel Zwinglio <samuelzwinglio@gmail.com>
2022-08-21 10:50:14 -0300

    fix back link

--- pages/dashboard/create-week.vue
+++ pages/dashboard/create-week.vue
@@ -3,7 +3,7 @@
     <div class="container mt-4">
       <div class="row justify-content-center">
         <div class="col-lg-6">
-          <a href="/dashboard" class="btn btn-secondary">Voltar</a>
+          <NuxtLink to="/dashboard" class="btn btn-secondary">Voltar</NuxtLink>
           <h1 class="mt-4">Criar uma nova semana</h1>
           <DashCreateWeekForm />
         </div>
